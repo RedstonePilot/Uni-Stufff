e65a7ea42e2810b566170c15abce25eaa5c954a8
Ben Walker <146777065+RedstonePilot@users.noreply.github.com>
2024-01-13 11:29:08 +0000

    Test commit

--- Semester_1/week11/turtlefunctionstemplate.py
+++ Semester_1/week11/turtlefunctionstemplate.py
@@ -12,14 +12,8 @@ my_turtle.showturtle()
 ####################      WRITE YOUR CODE BELOW      #########################
 
 
-
-
-
-
-
-
 #################### WRITE YOUR CODE ABOVE THIS LINE #########################
 ####################        IGNORE CODE BELOW        #########################
 
-## Must be the last line of code 
+# Must be the last line of code
 my_turtle.screen.exitonclick()
