17e15f821aa2b4fe1c9ab18728c3b523591ba4da
Ben Walker <146777065+RedstonePilot@users.noreply.github.com>
2024-01-19 13:55:16 +0000

    done

---
+++ Semester_1/Software Asessment/question_1.py
@@ -0,0 +1,18 @@
+def string_pattern(size):
+    if size <= 2:
+        raise ValueError("Size must be larger than or equal to 3")
+
+    output = [f"+{'-'*(size-2)}+\n"]
+    for i in range(1, size//2):
+        output.append(f"{'-'*(i)}+{'-'*(size-(2+i*2))}+{'-'*(i)}\n")
+
+    first_half = output.copy()
+    first_half.reverse()
+    if size % 2 == 1:
+        output.append(f"{'-'*(size//2)}+{'-'*(size//2)}\n")
+
+    output += first_half
+    return "".join(output)
+
+
+print(string_pattern(3))
